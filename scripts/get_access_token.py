cc3b1ff8d27a75ea285b453339813ada2a850980
AlexanderPease <alexander@usv.com>
2014-03-06 16:06:36 -0500

    ending dallas. added get_access_token but it's not configured for tornado

---
+++ scripts/get_access_token.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+#
+# Copyright 2007-2013 The Python-Twitter Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# parse_qsl moved to urlparse module in v2.6
+try:
+    from urlparse import parse_qsl
+except:
+    from cgi import parse_qsl
+
+import webbrowser
+import oauth2 as oauth
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twittercongress.settings")
+from congress_app.models import Twitter_FTV, Politician
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+CONSUMER_KEY = 'hNxtR1bjU2QnJqQZYftUzA'
+CONSUMER_SECRET = 'nXVHf7tiGzVvfrGA3VRSbdvjIIt1H706tjiP9rK2o4'
+
+''' Gets access key and token for the Twitter user currently logged into default browser '''
+def get_access_token(consumer_key, consumer_secret):
+
+    signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+    oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+    oauth_client = oauth.Client(oauth_consumer)
+
+    print 'Requesting temp token from Twitter'
+
+    resp, content = oauth_client.request(REQUEST_TOKEN_URL, 'POST', body="oauth_callback=oob")
+
+    if resp['status'] != '200':
+        print 'Invalid respond from Twitter requesting temp token: %s' % resp['status']
+    else:
+        request_token = dict(parse_qsl(content))
+        url = '%s?oauth_token=%s' % (AUTHORIZATION_URL, request_token['oauth_token'])
+
+        print ''
+        print 'I will try to start a browser to visit the following Twitter page'
+        print 'if a browser will not start, copy the URL to your browser'
+        print 'and retrieve the pincode to be used'
+        print 'in the next step to obtaining an Authentication Token:'
+        print ''
+        print url
+        print ''
+
+        webbrowser.open(url)
+        pincode = raw_input('Pincode? ')
+
+        token = oauth.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+        token.set_verifier(pincode)
+
+        print ''
+        print 'Generating and signing request for an access token'
+        print ''
+
+        oauth_client = oauth.Client(oauth_consumer, token)
+        resp, content = oauth_client.request(ACCESS_TOKEN_URL, method='POST', body='oauth_callback=oob&oauth_verifier=%s' % pincode)
+        access_token = dict(parse_qsl(content))
+
+        if resp['status'] != '200':
+            print 'The request for a Token did not succeed: %s' % resp['status']
+            print access_token
+        else:
+            print 'Access Token key: %s' % access_token['oauth_token']
+            print 'Access Token secret: %s' % access_token['oauth_token_secret']
+            print ''
+            return access_token['oauth_token'], access_token['oauth_token_secret']
+
+def main():
+    # Check to see if Twitter_FTV model exists, or else create
+    handle = raw_input('Twitter_FTV handle: ')
+    try:
+        twitter_ftv = Twitter_FTV.objects.get(handle=handle)
+    except:
+        # Identify desired politician to link account to
+        print "Model does not yet exist. Please enter additional info to create:"
+        last_name = raw_input('Last name of politician: ')
+        politicians = Politician.objects.filter(last_name__contains=last_name)
+        if len(politicians) == 0:
+            print "No politicians found, please try again"
+            return
+        elif len(politicians) == 1:
+            p_id = politicians[0].id
+        else:  
+            for p in politicians:
+                print "%s %s: ID %s" % (p.first_name, p.last_name, p.id)
+            p_id = raw_input('ID of correct politician (see above): ')
+
+        # Finally create the account
+        try: 
+            twitter_ftv = Twitter_FTV.objects.create(handle=handle, politician_id=p_id)
+            print 'Model successfully created'
+        except:
+            'Model could not be created'
+            return
+
+    
+    # Check for other, non-necessary fields
+    if not twitter_ftv.email:
+        code = raw_input('Enter code (to create email address and password): ')
+        twitter_ftv.email = code + "@followthevote.org"
+        twitter_ftv.email_password = "statueofliberty" + code
+    twitter_ftv.save()
+
+    # OAuth
+    print 'Connecting to Twitter...'
+    if not twitter_ftv.access_key or not twitter_ftv.access_secret:
+        access_key, access_secret = get_access_token(CONSUMER_KEY, CONSUMER_SECRET)
+        twitter_ftv.access_key = access_key
+        twitter_ftv.access_secret = access_secret
+        twitter_ftv.save()
+    else:
+        'Already OAuthed to Twitter'
+
+    # Get Twitter account id
+    if not twitter_ftv.user_id:
+        api = twitter_ftv.login()
+        user = api.GetUser(screen_name = twitter_ftv.handle) # user is a class from python-twitter
+        twitter_ftv.user_id = user.id
+        twitter_ftv.save()
+
+    print 'Model complete!'
+
+
+if __name__ == "__main__":
+    main()
--- scripts/update_ftv_accounts.py
+++ scripts/update_ftv_accounts.py
@@ -7,8 +7,22 @@ except:
     pass
 
 from db import politiciandb
+import twitter as python_twitter
 
 
 ''' Makes sure @FollowTheVote is following all FTV accounts '''
-def update_follow_the_vote():
-	
+def update_ftv_twitter():
+	print settings.get('twitter_consumer_key')
+	print settings.get('ftv_twitter_consumer_key')
+	api = python_twitter.Api(consumer_key=settings.get('twitter_consumer_key'),
+                consumer_secret=settings.get('twitter_consumer_secret'),
+                access_token_key="2242697833-DiyEb0L6YvGo2zCApo1NSgay9Ul5DpZNOAOPyLR",
+                access_token_secret="JXtFJTvsI1KgyDggamOxUd54AHoBABoatO1x5lYytZTnH")
+                #access_token_key=settings.get('ftv_twitter_consumer_key'),
+                #access_token_secret=settings.get('ftv_twitter_access_secret'))
+	print api
+
+def main():
+    update_ftv_twitter()
+
+if  __name__ =='__main__':main()
